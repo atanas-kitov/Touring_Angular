ec7595cae0e034f2edfb3c5e769ffa8967186574
Atanas Kitov <atanaskitov@gmail.com>
2023-04-06 13:51:37 +0300

    resolving problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
