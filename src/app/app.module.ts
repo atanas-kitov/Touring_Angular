735cf48f4dfe8825119ea5f779cb0f3ac0ea3e6d
Atanas Kitov <atanaskitov@gmail.com>
2023-04-06 17:36:26 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationComponent } from './header/navigation/navigation.component';
+import { HeroImgComponent } from './body/hero-img/hero-img.component';
+import { AboutComponent } from './body/about/about.component';
+import { ContactComponent } from './body/contact/contact.component';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
+    HeroImgComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/navigation/navigation.component.ts
+++ src/app/header/navigation/navigation.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
+  public onClick() : void {
+    console.log('clicked')
+  }
 
 }
